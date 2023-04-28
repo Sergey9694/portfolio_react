25e52c0bb588b1d6fee641bfbd09230ebba24d5f
Sergey Drobilenko <drobilenko94@mail.ru>
2023-04-28 20:07:38 +0300

    navbar component created and styles assigned

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
+import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
-    return <div></div>;
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <img src={images.logo} alt="logo" />
+            </div>
+            <ul className={styles.links}>
+                {[
+                    "Домашняя",
+                    "Обо мне",
+                    "Мои работы",
+                    "Навыки",
+                    "Контакты",
+                ].map((item) => (
+                    <li className={styles.link} key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
