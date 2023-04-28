f4b98ec53b604a9482f4a604c5a7237521f6e742
Sergey Drobilenko <drobilenko94@mail.ru>
2023-04-28 21:15:56 +0300

    added burger menu and state management for burger

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { images } from "../../constants";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
@@ -5,6 +7,8 @@ import { motion } from "framer-motion";
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>
@@ -24,6 +28,36 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+            <div className={styles.burger}>
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }} //ширина 300 по x
+                        transition={{ duration: 0.85, ease: "easeOut" }} //продолжительность 0.85с
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                            {[
+                                "Домашняя",
+                                "Обо мне",
+                                "Мои работы",
+                                "Навыки",
+                                "Контакты",
+                            ].map((item) => (
+                                <li key={item}>
+                                    <a
+                                        href={`#${item}`}
+                                        onClick={() => setToggle(false)}
+                                    >
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
         </nav>
     );
 };
