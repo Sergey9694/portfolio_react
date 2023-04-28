b71324563c29d276d67ae402995e202990a9a121
Sergey Drobilenko <drobilenko94@mail.ru>
2023-04-28 18:34:20 +0300

    added images and styles for app

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,9 +1,11 @@
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
+import styles from "./App.module.scss";
+
 function App() {
     return (
-        <div className="app">
+        <div className={styles.app}>
             <Navbar />
             <Header />
             <About />
