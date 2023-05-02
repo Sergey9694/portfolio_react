d64d6e9b745522ce29b029e182037ce1597563dd
Sergey Drobilenko <drobilenko94@mail.ru>
2023-05-02 19:35:36 +0300

    about component written and styles and adaptive for mobile devices are written

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,67 @@
+import { useState, useEffect } from "react";
+
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
 import styles from "./About.module.scss";
 
+const abouts = [
+    {
+        title: "Разработка  одностраничных лендингов",
+        description: "Продукты, созданные при помощи HTML/CSS/JavaScript",
+        imgUrl: images.about01,
+    },
+    {
+        title: "Разработка  многостраничных сайтов",
+        description:
+            "Продукты, созданные при помощи HTML/CSS/JavaScript(возможно применение Bootstrap, Material UI и др.)",
+        imgUrl: images.about02,
+    },
+    {
+        title: "Разработка веб приложений",
+        description: "Продукты, созданные при помощи библиотеки React и др.",
+        imgUrl: images.about03,
+    },
+];
+
 const About = () => {
-    return <div></div>;
+    return (
+        <>
+            <h2 className={styles.title}>
+                <span>Хороший сайт - </span>
+                Залог успеха <br />
+                <span> для вашего Бизнеса</span>
+            </h2>
+
+            <div className={styles.profile}>
+                {abouts.map((about, index) => {
+                    return (
+                        <motion.div
+                            whileInView={{ opacity: 1 }}
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: 0.5, type: "tween" }}
+                            className={styles.item}
+                            key={about.title + index}
+                        >
+                            <img src={about.imgUrl} alt={about.title} />
+                            <h2
+                                className={styles.aboutTitle}
+                                style={{ marginTop: 20 }}
+                            >
+                                {about.title}
+                            </h2>
+                            <p
+                                className={styles.aboutText}
+                                style={{ marginTop: 10 }}
+                            >
+                                {about.description}
+                            </p>
+                        </motion.div>
+                    );
+                })}
+            </div>
+        </>
+    );
 };
 
 export default About;
